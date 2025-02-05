bf696e14340f668a8fb2d33be601a8d2ecdc9f59
kenny <97374837+kankenny@users.noreply.github.com>
2025-02-05 16:56:23 +0000

    hotfix: remove whitespace on URLS

--- dreamify/deep_dream.py
+++ dreamify/deep_dream.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 def main():
 
-    url = "https://storage.googleapis.com/download.tensorflow.org/ \
+    url = "https://storage.googleapis.com/download.tensorflow.org/\
     example_images/YellowLabradorLooking_new.jpg"
 
     # Download an image and read it into a NumPy array.
@@ -34,7 +34,7 @@ def main():
     show(original_img)
     display.display(
         display.HTML(
-            'Image cc-by: <a "href=https://commons.wikimedia.org/wiki/ \
+            'Image cc-by: <a "href=https://commons.wikimedia.org/wiki/\
             File:Felis_catus-cat_on_snow.jpg">Von.grzanka</a>'
         )
     )
