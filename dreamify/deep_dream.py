e2d9cf45b94b6479fb1c52f204f3b24bb17f7da0
kenny <97374837+kankenny@users.noreply.github.com>
2025-02-13 23:41:31 +0000

    fix: fix dtype of img size tensor

--- dreamify/deep_dream.py
+++ dreamify/deep_dream.py
@@ -137,11 +137,12 @@ def deep_dream_rolled(
     img = tf.keras.applications.inception_v3.preprocess_input(img)
     initial_shape = img.shape[:-1]
     img = tf.image.resize(img, initial_shape)
-
+    
     for octave in octaves:
         new_size = tf.cast(tf.convert_to_tensor(base_shape[:-1]), tf.float32) * (
             octave_scale**octave
         )
+        new_size = tf.cast(new_size, tf.int32)
         img = tf.image.resize(img, tf.cast(new_size, tf.int32))
 
         for iteration in range(iterations):
