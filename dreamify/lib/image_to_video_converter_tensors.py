2ab04d16fa0086e4f902a4fcd3f05797d18d9897
kenny <97374837+kankenny@users.noreply.github.com>
2025-02-14 23:31:17 +0000

    feat: implement adaptive fps

--- dreamify/lib/image_to_video_converter_tensors.py
+++ dreamify/lib/image_to_video_converter_tensors.py
@@ -5,12 +5,14 @@ from moviepy.video.VideoClip import DataVideoClip
 from dreamify.utils.common import deprocess
 
 
-class ImageToVideoConverter:
+class ImageToVideoConverterNumpy:
     def __init__(self, dimensions, max_frames_to_sample):
         self.dimensions = dimensions
         self.frames_for_vid: list = []
         self.max_frames_to_sample: int = max_frames_to_sample
         self.curr_frame_idx: int = 0
+        self.num_frames_to_insert:int = 0
+        self.FPS:int = 30
 
     def add_to_frames(self, frame):
         frame = tf.image.resize(frame, self.dimensions)
@@ -22,21 +24,22 @@ class ImageToVideoConverter:
     def continue_framing(self):
         return self.curr_frame_idx < self.max_frames_to_sample - 1
 
-    def to_video(self, output_path, duration, mirror_video, fps=60):
+    def to_video(self, output_path="dream.mp4", duration=3, mirror_video=False):
+        self.duration = duration
+        self.num_frames_to_insert = self.calculate_num_frames_to_insert()
+
         self.upsample()
 
         frames = [frame.numpy() for frame in self.frames_for_vid]
         print(f"Number of images to frame: {len(frames)}")
 
-        vid = DataVideoClip(frames, lambda x: x, fps=fps)
+        vid = DataVideoClip(frames, lambda x: x, fps=self.FPS)
         if mirror_video:
             vid = TimeSymmetrize().apply(vid)
         vid = AccelDecel(new_duration=duration).apply(vid)
         vid.write_videofile(output_path)
 
     def upsample(self):
-        NUM_FRAMES_TO_INSERT = 30
-
         new_frames = []
 
         # Upsample via frame-frame interpolation
@@ -47,7 +50,7 @@ class ImageToVideoConverter:
             # Add original frame
             new_frames.append(self.frames_for_vid[i])
 
-            interpolated = self.interpolate_frames(frame1, frame2, NUM_FRAMES_TO_INSERT)
+            interpolated = self.interpolate_frames(frame1, frame2, self.num_frames_to_insert)
             new_frames.extend(interpolated)
 
         new_frames.extend(
@@ -65,3 +68,15 @@ class ImageToVideoConverter:
             :, None, None, None
         ] * frame2
         return tf.cast(interpolated_frames, tf.uint8)
+
+    def calculate_num_frames_to_insert(self):
+        """
+        Calculate the number of frames to interpolate to ensure 30fps of the video
+
+        Derivation: 
+                30 = (max_frames_to_sample * num_frames_to_insert) // duration
+             => 30 * duration = max_frames_to_sample * num_frames_to_insert
+             => 30 * duration // max_frames_to_sample = num_frames_to_insert
+             ≡ num_frames_to_insert = (30 * duration) // max_frames_to_sample 
+        """
+        return (self.FPS * self.duration) // self.max_frames_to_sample
