019ba76bda6940836f62b33f39d8937300b2d378
kenny <97374837+kankenny@users.noreply.github.com>
2025-02-21 20:50:57 +0000

    refactor: reduce redundant enum lookups

--- dreamify/lib/models/base/base_models.py
+++ dreamify/lib/models/base/base_models.py
@@ -31,26 +31,24 @@ MODEL_MAP = {
 }
 
 
-def get_layer_settings(model_name: str, dream_style="deep", layer_settings=None):
-    model_name_enum = ModelType[model_name.upper()]
-
-    if layer_settings is None and dream_style == "deep":
-        layer_settings = DeepDreamModelLayerSettings[model_name_enum.name].value
-    elif layer_settings is None and dream_style == "shallow":
-        layer_settings = ShallowDreamModelLayerSettings[model_name_enum.name].value
-    else:
-        raise NotImplementedError()
-    return layer_settings
+def get_layer_settings(model_name_enum: ModelType, dream_style="deep"):
+    if dream_style == "deep":
+        return DeepDreamModelLayerSettings[model_name_enum.name].value
+    elif dream_style == "shallow":
+        return ShallowDreamModelLayerSettings[model_name_enum.name].value
+    raise NotImplementedError()
 
 
 def choose_base_model(model_name: str, dream_style="deep", layer_settings=None):
     if model_name in ["random", "any"]:
-        model_name = random.choice([model.value for model in ModelType])
-    model_name_enum = ModelType[model_name.upper()]
+        model_name_enum = random.choice(list(ModelType))
+    else:
+        model_name_enum = ModelType[model_name.upper()]
 
     model_fn = MODEL_MAP[model_name_enum]
     base_model = model_fn(weights="imagenet", include_top=False)
 
-    layer_settings = get_layer_settings(model_name, dream_style, layer_settings)
+    if layer_settings is None:
+        layer_settings = get_layer_settings(model_name_enum, dream_style)
 
     return base_model, layer_settings
